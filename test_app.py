8e933392f64747a4219c2f1bb8b73516a80c74ae
Diego Gómez <dagomez2530@gmail.com>
2022-03-24 09:00:32 -0500

    Cambio de letra

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 def hello_world(message = "Hello World!"):
     if message:
-        print(message, "pront from feature_ŕ")
+        print(message, "pront from feature_pr")
     return message, "conflicto rama feature_cambio_conflicto"
 
 saludo = hello_world()
